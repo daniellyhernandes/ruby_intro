e597c5194aa5a6fcd7e092083a3aab25504005c8
Danielly Hernandes <danielly.hernandes@kellogg.northwestern.edu>
2023-04-20 04:28:05 +0000

    implemented looping using loop do

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -9,27 +9,65 @@
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
-index = 0
+# index = 0
+# loop do
+#     if index == ranks.size * suits.size
+#         break
+#     end
+#     ranks_index = index % ranks.size # --> 0, 1, 2, 3, 4,...,12, 0, 1, ...12 
+#     suits_index = index / ranks.size # --> 0, 0, .....0, 1, ....1, ...2, 
+#     rank = ranks[ranks_index]
+#     suit = suits[suits_index]
+#     puts "#{rank} of #{suit}"
+    
+#     index = index + 1    
+# end
+
+# ranks_index = 0
+# loop do
+#     if ranks_index == ranks.size
+#         break
+#     end
+#     suits_index = 0
+#     loop do
+#         if suits_index == suits.size
+#             break
+#         end
+#     rank = ranks[ranks_index]
+#     suit = suits[suits_index]
+#     puts "#{rank} of #{suit}"
+    
+#     suits_index = suits_index + 1    
+#     end
+#     ranks_index = ranks_index +1
+# end
+
+# for zebra in ranks
+#     for suit in suits  
+#         puts "#{zebra} of #{suit}"
+#     end
+#     end
+
+ranks_index = 0
 loop do
-    if index == 52
-        break
-    end
-rank\-index = 52 
-    rank = ranks[index]
-    suit = suits[index]
-    puts "#{rank} of Clubs"
-    puts "#{rank} of Diamonds"
-    puts "#{rank} of Hearts"
-    puts "#{rank} of Spades"
+   if  ranks_index == ranks.size
+       break
+   end
+   suits_index = 0
+    loop do
+        if suits_index == suits.size
+            break
+        end
+    rank = ranks[ranks_index]
+    suit = suits[suits_index]
+    puts "#{rank} of #{suit}"
 
-    index = index + 1    
+    suits_index = suits_index + 1
+
+    end
+    ranks_index = ranks_index + 1
+
 end
-
-for rank in ranks
-    for suit in suits  
-        puts "#{rank} of #{suit}"
-    end
-    end
 
 # Sample output:
 # 2 of Clubs
@@ -37,7 +75,7 @@ for rank in ranks
 # 2 of Hearts
 # 2 of Spades
 # 3 of Clubs
-# ...
+# ...   
 
 # CHALLENGE
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
